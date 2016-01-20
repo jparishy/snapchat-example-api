03fcd9f4f98aef6f351906962b4b969dea91b487
Julius Parishy <hello@juliusparishy.com>
2016-01-20 00:02:07 -0500

    add to_user to snap json

--- app/models/snap.rb
+++ app/models/snap.rb
@@ -3,6 +3,6 @@ class Snap < ActiveRecord::Base
 	belongs_to :to_user, class_name: "User"
 
 	def json
-		{ id: self.id, image_url: self.image_url, from_user: self.from_user.json, unread: self.unread, created_at: self.created_at }
+		{ id: self.id, image_url: self.image_url, to_user: self.to_user.json, from_user: self.from_user.json, unread: self.unread, created_at: self.created_at }
 	end
 end
